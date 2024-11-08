7b7f688629ed66894951596beaca3bc4576a95f9
rajagopalreddy-tm <rajagopal.reddy@trainingmug.com>
2024-11-08 16:08:24 +0530

    working-with-static-methods program

---
+++ src/com/trainingmug/java/StaticTest.java
@@ -0,0 +1,29 @@
+package com.trainingmug.java;
+
+public class StaticTest {
+    public static void main(String[] args) {
+        // Static Variable
+        System.out.println("Company Name : " + Employee.companyName);
+
+        /*
+        Employee employee1 = new Employee();
+        System.out.println("Company Name : " + employee1.companyName); // Not Recommended
+        employee1.companyName = "TrainingMug Pvt Ltd";
+
+        Employee employee2 = new Employee();
+        System.out.println("Company Name : " + employee2.companyName); // Not Recommended
+
+        System.out.println("Company Name : " + Employee.companyName);
+
+         */
+
+        // static method
+        Employee.companyInfo();
+
+        /*
+        Employee employee1 = new Employee();
+        employee1.companyInfo(); // Not Recommended
+
+         */
+    }
+}
